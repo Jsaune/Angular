f6a68db834d044c3f8da3cd86409a4510dee291d
Jsp-project956 <jsaune@senati.pe>
2025-08-11 19:45:33 -0500

    11/08/25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaProductosComponent } from "./lista-productos/lista-productos.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-holi',
+  imports: [
+    RouterOutlet,
+    ListaProductosComponent 
+   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
 export class AppComponent {
-  title = 'Primer_proyecto';
+  title:string = 'Titulo';
 }
